cf8bae6662d8eab138674a01b2675f68e08efaee
Martin <der_moaddin@yahoo.de>
2023-01-23 22:38:16 +0100

    aufgeräumt -
    .. in GANZ großen Anführungszeichen!

--- src/app/TechnicalConstants.ts
+++ src/app/TechnicalConstants.ts
@@ -1,7 +1,10 @@
 import path from "path";
 import fs from "fs";
 
+// Ort für Quelldateien im JSON-Format
 export const externalFileDir: string = path.resolve(path.resolve("./") + "/your_files_here/Polarsteps-Blogs");
+
+// Verzeichnis, in dem die fertige Word-Datei erzeugt wird
 export const resultDir: string = "result";
 export const resultFilenameUser: string = "MyUserDocument.docx";
 export const resultFilenameTrip: string = "MyTripDocument.docx";
